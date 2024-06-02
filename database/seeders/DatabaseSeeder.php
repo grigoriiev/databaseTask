75a7256ab995b02924770b60f263abb25376f8d1
Andrew <roliol27@yandex.ru>
2024-06-02 10:29:15 +0300

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,19 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        $flag=true;
-
-        while ($flag) {
-
-            $response = Http::get('http://89.108.115.241:6969/api/incomes?dateFrom=2020-05-31&dateTo=2029-05-31&page=1&key=E6kUTYrYwZq2tN4QEtyzsbEBk3ie&limit=500');
-
-
-
-
-            $flag=false;
-
-            dd($response);
-        }
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
